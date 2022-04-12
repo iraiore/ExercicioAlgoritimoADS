6a2fca1faaf1d67198d42175da18082f56ff709c
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-12 18:02:44 -0400

    foram criadas variaveis para armazenar o quadrado dos valores

--- atividade3.cs
+++ atividade3.cs
@@ -18,10 +18,10 @@ namespace atividade3
             int b = 0;
             int c = 0;
             int d = 0;
-            int aa = 0;
-            int bb = 0;
-            int cc = 0;
-            int dd = 0;
+            int a-quad = 0;
+            int b-quad = 0;
+            int c-quad = 0;
+            int d-quad = 0;
             //CÓDIGO
             //Sequencia de instrução para o usuario entrar com os dados e serem armazenadas nas variáveis
             Console.WriteLine("Entre como o valor 1: ");
@@ -32,14 +32,14 @@ namespace atividade3
             c = int.Parse(Console.ReadLine());
             Console.WriteLine("Entre como o valor 4: ");
             d = int.Parse(Console.ReadLine());
-            cc = c*c;
-            //dd = d*d;
-           //aa = a*a;
+            a-quad = a*a;
+            b-quad = b*b;
+            c-quad = c*c;
+            d-quad = d*d;
 
-            if (cc>=100)
+            if (c-quad>=1000)
             {
-                c = c*c;
-                Console.WriteLine("o quadrado do valor 3 é: "+c);
+                Console.WriteLine("o quadrado do valor 3 é: "+c-quad);
             }else
             {
                 Console.WriteLine("Os quatro valores de  entrada são: "+a+", "+b+", "+c+", "+d+".");
@@ -47,7 +47,7 @@ namespace atividade3
                 b =b*b;
                 c =c*c;
                 d =d*d;
-                Console.WriteLine("E seus respectivos quadrados são: "+a+", "+b+", "+c+", "+d+"."));
+                Console.WriteLine("E seus respectivos quadrados são: "+a-quad+", "+b-quad+", "+c-quad+", "+d-quad+"."));
             }
         }
     }
