8ec1f994ba690500c7e6b657e8a67ad9e97cc539
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-12 17:51:27 -0400

    primeira tentativa

--- atividade3.cs
+++ atividade3.cs
@@ -6,3 +6,42 @@ Se o valor resultante do quadrado do terceiro for >= 1000, imprima-o e finalize;
 Caso contrário, imprima os valores lidos e seus respectivos quadrados. 
 */
 
+using System;
+namespace atividade3
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            //DECLARAÇÃO DE VARIÁVEIS
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            //CÓDIGO
+            //Sequencia de instrução para o usuario entrar com os dados e serem armazenadas nas variáveis
+            Console.WriteLine("Entre como o valor 1: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre como o valor 2: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre como o valor 3: ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre como o valor 4: ");
+            d = int.Parse(Console.ReadLine());
+            
+            if (c*c>=100)
+            {
+                c = c*c;
+                Console.WriteLine("o quadrado do valor 3 é: "+c);
+            }else
+            {
+                Console.WriteLine("Os quatro valores de  entrada são: "+a+", "+b+", "+c+", "+d+".");
+                a =a*a;
+                b =b*b;
+                c =c*c;
+                d =d*d;
+                Console.WriteLine("E seus respectivos quadrados são: "+a+", "+b+", "+c+", "+d+"."));
+            }
+        }
+    }
+}
