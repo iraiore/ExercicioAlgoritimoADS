49c0c44c36798ac8714c774f9223101581c1fca2
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-12 09:54:06 -0400

    teste

---
+++ atividade1.cs
@@ -0,0 +1,12 @@
+using System;
+name space atividade1
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello Word")
+
+        }
+    } 
+}
