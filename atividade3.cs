1a3cda218cb6e4bc7776cc47acd9720db749b961
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-12 18:05:33 -0400

    nano pode usar hifen ao declarar uma uma variavel

--- atividade3.cs
+++ atividade3.cs
@@ -18,10 +18,10 @@ namespace atividade3
             int b = 0;
             int c = 0;
             int d = 0;
-            int a-quad = 0;
-            int b-quad = 0;
-            int c-quad = 0;
-            int d-quad = 0;
+            int a_quad = 0;
+            int b_quad = 0;
+            int c_quad = 0;
+            int _quad = 0;
             //CÓDIGO
             //Sequencia de instrução para o usuario entrar com os dados e serem armazenadas nas variáveis
             Console.WriteLine("Entre como o valor 1: ");
@@ -32,14 +32,14 @@ namespace atividade3
             c = int.Parse(Console.ReadLine());
             Console.WriteLine("Entre como o valor 4: ");
             d = int.Parse(Console.ReadLine());
-            a-quad = a*a;
-            b-quad = b*b;
-            c-quad = c*c;
-            d-quad = d*d;
+            a_quad = a*a;
+            b_quad = b*b;
+            c_quad = c*c;
+            d_quad = d*d;
 
-            if (c-quad>=1000)
+            if (c_quad>=1000)
             {
-                Console.WriteLine("o quadrado do valor 3 é: "+c-quad);
+                Console.WriteLine("o quadrado do valor 3 é: "+c_quad);
             }else
             {
                 Console.WriteLine("Os quatro valores de  entrada são: "+a+", "+b+", "+c+", "+d+".");
@@ -47,7 +47,7 @@ namespace atividade3
                 b =b*b;
                 c =c*c;
                 d =d*d;
-                Console.WriteLine("E seus respectivos quadrados são: "+a-quad+", "+b-quad+", "+c-quad+", "+d-quad+"."));
+                Console.WriteLine("E seus respectivos quadrados são: "+a_quad+", "+b_quad+", "+c_quad+", "+d_quad+"."));
             }
         }
     }
