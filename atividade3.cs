26ca432af6d438e5919281adbee89275f49fadcc
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-12 17:33:47 -0400

    Update atividade3.cs

--- atividade3.cs
+++ atividade3.cs
@@ -1,2 +1,8 @@
-
+/*
+Desenvolva um algoritmo que:
+Leia 4 (quatro) números; 
+Calcule o quadrado de cada um; 
+Se o valor resultante do quadrado do terceiro for >= 1000, imprima-o e finalize; 
+Caso contrário, imprima os valores lidos e seus respectivos quadrados. 
+*/
 
