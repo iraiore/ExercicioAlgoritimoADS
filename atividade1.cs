ecb5d657f18c06db2da8900d31336232c66495bb
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-12 10:51:38 -0400

    Pergunta
    Adicionando a pergunta ao código dentro do comentário.  Para então escrever o código  e resolver o problema

--- atividade1.cs
+++ atividade1.cs
@@ -1,3 +1,12 @@
+/*
+João Papo-de-Pescador, homem de bem, comprou um microcomputador para 
+controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de 
+peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo 
+(50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que 
+você faça programa que leia a variável P (peso de peixes) e verifique se há excesso. 
+Se houver, gravar na variável E (Excesso) e na variável M o valor da multa que João 
+deverá pagar. Caso contrário mostrar tais variáveis com o conteúdo ZERO.
+*/
 using System;
 namespace atividade1
 {
