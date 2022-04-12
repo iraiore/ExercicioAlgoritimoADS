ecbdab8a13fcd24af59add6c9b2b2db52e690fa9
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-12 17:58:05 -0400

    foi tirada o operação de denro do if

--- atividade3.cs
+++ atividade3.cs
@@ -18,6 +18,10 @@ namespace atividade3
             int b = 0;
             int c = 0;
             int d = 0;
+            int aa = 0;
+            int bb = 0;
+            int cc = 0;
+            int dd = 0;
             //CÓDIGO
             //Sequencia de instrução para o usuario entrar com os dados e serem armazenadas nas variáveis
             Console.WriteLine("Entre como o valor 1: ");
@@ -28,8 +32,11 @@ namespace atividade3
             c = int.Parse(Console.ReadLine());
             Console.WriteLine("Entre como o valor 4: ");
             d = int.Parse(Console.ReadLine());
-            
-            if (c*c>=100)
+            cc = c*c;
+            //dd = d*d;
+           //aa = a*a;
+
+            if (cc>=100)
             {
                 c = c*c;
                 Console.WriteLine("o quadrado do valor 3 é: "+c);
