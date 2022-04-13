e4ee000598856c35a4501931e16a60ba9a46c409
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-13 15:56:37 -0400

    tentativa 1 de rodar o codigo

--- atividade6.cs
+++ atividade6.cs
@@ -5,3 +5,34 @@ Infantil B = 8 a 11 anos
 Juvenil A = 12 a 13 anos 
 Juvenil B = 14 a 17 anos 
 Adultos = Maiores de 18 anos*/
+
+using System;
+namespace atividade6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+            Console.WriteLine("Entre com a idade do nadador: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade  = 5 && idade <= 7)
+            {
+                Console.WriteLine("A classificação do nadador é: Infantil A");
+            }else if (idade = 8 && idade <= 11)
+            {
+                Console.WriteLine("A classificação do nadador é: Infantil B");
+            }else if (idade = 12 && idade <= 13)
+            {
+                Console.WriteLine("A classificação do nadador é: Juvenil A");
+            }else if (idade = 14 && idade <= 17)
+            {
+                Console.WriteLine("A classificação do nadador é: Juvenil B");
+            }else
+            {
+                Console.WriteLine("A classificaçãodo nadador é: Adultos");
+            }
+        }
+    }
+}
