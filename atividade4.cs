87160cd46e847c45452f4aa100f73ac7c7bfb950
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-13 14:03:56 -0400

    tentativa 1 de rodar o codigo

--- atividade4.cs
+++ atividade4.cs
@@ -1,10 +1,31 @@
 /*Faça um algoritmo que leia um número inteiro e mostre uma mensagem indicando 
-se este número é par ou impar, e se é positivo ou negativo. 
-5) A Secretaria de Meio Ambiente que controla o índice de poluição mantém 3 grupos 
-de indústrias que são altamente poluentes do meio ambiente. O índice de poluição 
-aceitável varia de 0,05 até 0,25. Se o índice sobe para 0,3 as indústrias do 1o grupo 
-são intimadas a suspenderem suas atividades, se o índice crescer para 0,4 as industrias 
-do 1o e 2o grupo são intimadas a suspenderem suas atividades, se o índice atingir 0,5 
-todos os grupos devem ser notificados a paralisarem suas atividades. Algoritmo que 
-leia o índice de poluição medido e emita a notificação adequada aos diferentes grupos 
-de empresas.*/
+se este número é par ou impar, e se é positivo ou negativo.*/
+
+using System;
+namespace atividade4
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Entre com o valor: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            
+            if(num % 2 == 0 && num >= 0)
+             {
+                Console.WriteLine("O número "+num+" é par positivo");
+             }else if(num % 2 == 0 && num < 0)
+                    {
+                        Console.WriteLine("O número "+num+" é par negativo");
+                    }else if(num % 2 == 1 && num < 0)
+                            {
+                        Console.WriteLine("O número "+num+" é impar negativo");
+                            }else if(num % 2 == 1 && num >= 0)
+                                    {
+                                        Console.WriteLine("O número "+num+" é impar positivo");
+                                    }
+        
+        }
+    }
+}
