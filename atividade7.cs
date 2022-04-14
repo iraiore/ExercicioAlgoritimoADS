b55b481c8617ced36cd5b902baab280b2f73f5aa
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-14 11:34:58 -0400

    testando parte do codigo

--- atividade7.cs
+++ atividade7.cs
@@ -21,15 +21,43 @@ namespace atividade7
             a =  int.Parse(Console.ReadLine());
             Console.WriteLine("Entre como o valor 2: ");
             b =  int.Parse(Console.ReadLine());
-            if(a>b)
+            if(a > b)
             {
                 maior = a;
                 menor = b;
-            }else if(a<b)
+            }else if(a <= b)
             {
                 maior = b;
                 menor = a;
             }
+            Console.WriteLine("Entre como o valor 3: ");
+            c =  int.Parse(Console.ReadLine());
+            if(c > maior)
+            {
+                maior = c;
+            }else if(c < menor )
+            {
+                menor = c;
+            }
+            Console.WriteLine("Entre como o valor 4: ");
+            d =  int.Parse(Console.ReadLine());
+            if(d > maior)
+            {
+                maior = d;
+            }else if(d < menor )
+            {
+                menor = d;
+            }
+            Console.WriteLine("Entre como o valor 5: ");
+            c =  int.Parse(Console.ReadLine());
+            if(e > maior)
+            {
+                maior = e;
+            }else if(e < menor )
+            {
+                menor = e;
+            }
+            Console.WriteLine(maior+" "+menor);
         }
     }
 }
