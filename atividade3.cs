a2d7ceec7f4ff25b68e88663e08f99d99fe0066b
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-20 21:18:58 -0400

    alteração teste

--- atividade3.cs
+++ atividade3.cs
@@ -3,7 +3,7 @@ Desenvolva um algoritmo que:
 Leia 4 (quatro) números; 
 Calcule o quadrado de cada um; 
 Se o valor resultante do quadrado do terceiro for >= 1000, imprima-o e finalize; 
-Caso contrário, imprima os valores lidos e seus respectivos quadrados. 
+Caso contrário, imprima os valores lidos e seus respectivos quadrados.mais teste 
 */
 
 using System;
